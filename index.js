2d028b2e903c9c631e32bfadfdc4162cfa2b0032
KINGSMON74 <mnjsaeed@gmail.com>
2024-05-12 18:49:10 +0330

    12

--- index.js
+++ index.js
@@ -18,23 +18,39 @@ io.on('connection', socket => {
     users.push({ User_Id: userId, Socket_Id: socketId })
 
     console.log(`a new socket connection (${userId})`);
+    // Join A Person To A New Room
+    socket.on('join-room', (event) => {
+        socket.join(`ROOMID:: ${event.roomId}`)
+        console.log(`user ${userId} Join to a room ${event.roomId}`);
+
+    })
+
+    // leave A Person from a Room
+    socket.on('leave-room', (event) => {
+        socket.leave(`ROOMID:: ${event.roomId}`)
+        console.log(`user ${userId} left from room ${event.roomId}`);
+
+    })
+
+    //Send Message indiviual 
 
     socket.on('send-message', (event) => {
         console.log(`user ${userId} sent a message to ${event.to} > ${event.message}`);
-        const filteredUsers = users.filter((elem)=>elem.User_Id == event.to )
-    
-        const receiverSocketId = filteredUsers[0].Socket_Id
-        socket.broadcast.to(receiverSocketId).emit('onMessage',{
-            'message':event.message,
-            'from':userId})
-            
-    })
+        const filteredUsers = users.filter((elem) => elem.User_Id == event.to)
 
+        const receiverSocketId = filteredUsers[0].Socket_Id
+        socket.broadcast.to(receiverSocketId).emit('onMessage', {
+            'message': event.message,
+            'from': userId
+        })
+
+    })
+    //Discconnect
     socket.on('disconnect', (event) => {
         console.log(`user (${userId}) disconnected!`);
-        const index = users.filter((elem)=>elem.userid == userId )
+        const index = users.filter((elem) => elem.userid == userId)
 
-        users.slice(index,1)
+        users.slice(index, 1)
     })
 })
 
